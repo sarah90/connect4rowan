5b7dfb3d01eca8908993822bdc606ae23bc98ded
bp2070 <bp2070@eb334574-0524-11de-b8f3-b9fb9eb71b04>
2009-03-11 22:38:00 +0000

    initial import!

---
+++ Connect4/src/Tester.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author link3988
+ */
+public class Tester {
+    public static void main(String [] args)
+    {
+        Tester t = new Tester();
+        DrawBoard db = new DrawBoard();
+    }
+
+}
