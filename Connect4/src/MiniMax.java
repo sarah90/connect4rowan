3ab7943dafef11678bdaf5d4af71619d0d21c7ef
bp2070 <bp2070@eb334574-0524-11de-b8f3-b9fb9eb71b04>
2009-04-12 22:18:09 +0000

    implemented deep copy

--- Connect4/src/MiniMax.java
+++ Connect4/src/MiniMax.java
@@ -5,20 +5,20 @@
 public class MiniMax {
     final int NUM_COLS = 7;
     boolean col_full = true;
-    
+
     int expand(int[][] board, int depth, boolean playersturn)
     {
         int hvalue = 0;
         if(depth == 0)
         {
-            return my_heuristic(board); 
+            return my_heuristic(board, playersturn);
         }
         else
         {
             if(playersturn)
             {
-                hvalue = 0;            
-            }   
+                hvalue = 0;
+            }
             else
                 hvalue = Integer.MAX_VALUE;
             for(int j = 0; j < NUM_COLS; j++)
@@ -39,13 +39,13 @@ public class MiniMax {
             }
         }
         return hvalue;
-        
+
     }
-    
+
     public int minimax(int [] [] board, int depth, boolean player)
-    { 
+    {
         int min = Integer.MAX_VALUE;
-        int move = -1;
+        int h = -1;
         int temp = 0;
         for(int j = 0; j < NUM_COLS; j++)
         {
@@ -56,21 +56,22 @@ public class MiniMax {
                 if(temp < min)
                 {
                     min = temp;
-                    move = j;                   
+                    h = j;
                 }
                 board = unmove(board, j);
             }
-            
+
         }
-        return move;
-        
+        return h;
+
     }
-    
+
    private int[][] unmove(int[][] b, int j)
    {
         for(int i = 0; i < 6; i++){
             if(b[i][j] != 0){
                 b[i][j] = 0;
+                break;
             }
         }
         return b;
@@ -82,6 +83,7 @@ public class MiniMax {
                 if(player)
                     b[i][j] = 1;
                 else b[i][j] = 2;
+                break;
             }
         }
         return b;
@@ -93,11 +95,46 @@ public class MiniMax {
         else
             return false;
     }
-    
-    
-    private int my_heuristic(int[][] board )
+
+
+    private int my_heuristic(int[][] board, boolean playersturn)
     {
-        return (int)(Math.random()*10);
+        int h = 0;
+        int temp = 0;
+        int piece = 0;
+
+        if(playersturn)
+            piece = 1;
+        else piece = 2;
+
+        for(int i = 0; i < board.length -1; i++){
+            for(int j = 0; j < board.length -1; j++){
+                if(board[i][j] == piece){
+
+                    if(i == 0) temp+= 1;
+                    if(i == 1) temp += 2;
+                    if(i == 2) temp += 3;
+                    if(i == 3) temp += 4;
+                    if(i == 4) temp += 3;
+                    if(i == 5) temp += 2;
+                    if(i == 6) temp += 1;
+
+                    if(j == 0) temp += 1;
+                    if(j == 1) temp += 2;
+                    if(j == 2) temp += 3;
+                    if(j == 3) temp += 3;
+                    if(j == 4) temp += 2;
+                    if(j == 5) temp += 1;
+
+
+
+                }
+            }
+        }
+
+        return temp;
+
+
     }
-    
+
 }
